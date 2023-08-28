7ddf5b4c489ab823670265ec72fc909d53ea5848
Chien Trang Minh <chien.trang@katalon.com>
2023-08-28 10:25:14 +0700

    Add explain code sample usage

---
+++ Scripts/2. Explain Code/1. Simple explanation/Script1692937775913.groovy
@@ -0,0 +1,39 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.click(findTestObject('Page_CuraHomepage/btn_MakeAppointment'))
+
+WebUI.setText(findTestObject('Page_Login/txt_UserName'), Username)
+
+WebUI.setText(findTestObject('Page_Login/txt_Password'), Password)
+
+WebUI.click(findTestObject('Page_Login/btn_Login'))
+
+landingPage = WebUI.verifyElementPresent(findTestObject('Page_CuraAppointment/div_Appointment'), GlobalVariable.G_Timeout)
+
+/*
+ *  This code is used to perform a series of actions on a web page using Katalon Studio.
+ *
+ *  1. Click on the 'Make Appointment' button on the Cura Homepage.
+ *  2. Set the text of the 'Username' field on the Login page to the value of the variable 'Username'.
+ *  3. Set the text of the 'Password' field on the Login page to the value of the variable 'Password'.
+ *  4. Click on the 'Login' button on the Login page.
+ *  5. Verify if the 'Appointment' div is present on the landing page within the specified timeout period.
+ *
+ */
